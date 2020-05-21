c51dc9c8760c24327cc597591b5a31f160b9e29c
sourabh <sourabh7singh@gmail.com>
2020-05-21 13:19:08 +0530

    aws auth

--- settings.py
+++ settings.py
@@ -1,6 +1,11 @@
-PROD = False
+import os
+
+PROD = True
+
+ACCESS_ID = os.environ.get("AWS_ACCESS_ID")
+ACCESS_SECRET = os.environ.get("AWS_ACCESS_SECRET")
 
 if not PROD:
   ES_URL = "http://localhost:9200"
 else:
-  ES_URL = "https://search-discordbot-zk7q5t7pg6dw2dutpwkni7gpxy.us-east-2.es.amazonaws.com"
+  ES_URL = "https://search-discord-jcizn4xvaamzdcxm6xyydnltvy.us-east-2.es.amazonaws.com"
--- utils.py
+++ utils.py
@@ -2,13 +2,23 @@ import settings
 import requests
 import json
 
+from requests_aws4auth import AWS4Auth
+
+auth = None
+if settings.PROD:
+    auth = AWS4Auth(
+      settings.ACCESS_ID,
+      settings.ACCESS_SECRET,
+      'us-east-2', 'es'
+    )
+
 
 def save_in_db(author, data):
     """Saves string data in db"""
     url = f"{settings.ES_URL}/{author.id}/_doc"
     requests.post(url, json={
       "text": data
-    })
+    }, auth=auth)
 
 
 def search_in_db(author, data):
@@ -24,7 +34,8 @@ def search_in_db(author, data):
           }
         }
       }),
-      headers = {"Content-type": "application/json", "Accept": "text/plain"}
+      headers = {"Content-type": "application/json", "Accept": "text/plain"},
+      auth = auth
     )
     if res.status_code==200:
         return map(lambda x: x['_source']['text'], res.json()['hits']['hits'])
